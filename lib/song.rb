63d363255021c8b751e46d5ba7f4819f86d20e04
favorable-device-5158 <blee2125@gmail.com>
2019-09-09 19:39:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artists
-  @@genres
+  @@artists = {}
+  @@genres = {}
 
   def initialize(name, artist, genre)
     @@count += 1
@@ -11,6 +11,5 @@ class Song
     @artist= artist
     @genre= genre
   end
-
 
 end
